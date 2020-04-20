90894b15ea8e1527d8b368c0361299702371b593
trp87-rutgers <trp87@scarletmail.rutgers.edu>
2020-04-20 13:41:32 -0400

    ACK, DATA create and read done, need Hello, LS

--- packets.py
+++ packets.py
@@ -45,29 +45,29 @@ def readACK(pkt):
 	"""
 	header = pkt[0:8]
 	pkttype, seq, src, dest = struct.unpack('BBBB', header)
-	return [seq, src, dest]
+	return [pkttype, seq, src, dest]
 
 def createDatapkt(src, Ndest, Rdest, dest1, dest2, dest3, data):
 	"""
-	Type(1)|SEQ(1)|LEN(2)|NDEST(1)|RDEST(1)|DEST1(1)|DEST2(1)|DEST3(1)|DATA(1-1491)print(str(4).encode('utf-8'))
+	Type(1)|SEQ(1)|LEN(2)|SRC(1)|NDEST(1)|RDEST(1)|DEST1(1)|DEST2(1)|DEST3(1)|DATA(1-1491)print(str(4).encode('utf-8'))
 	Type = 3, 1B
 	SEQ generated randomly
 	pktlen = length of data input
 	Inputs:
 		src:
-		Ndest:
+		Ndest: K, 1-3
 		Rdest:
 		dest1:
 		dest2:
 		dest3:
 		data:
 	Outputs:
-
+		pkt:
 	"""
 
 	pktlen = len(data)
 	seq = random.randint(0,254) # random integer between 0 and 254 inclusive
-	header = struct.pack('BBHBBBBB'3, seq, pktlen, src, Ndest, Rdest, dest1, dest2, dest3)
+	header = struct.pack('BBHBBBBBB', 3, seq, pktlen, src, Ndest, Rdest, dest1, dest2, dest3)
 	return header + bytes(data, 'utf-8')
 
 def readDATApkt(pkt):
@@ -75,9 +75,14 @@ def readDATApkt(pkt):
 
 	Inputs:
 		pkt:
+	Outputs:
+		contents:
 	"""
-	header = pkt[0:]
+	header = pkt[0:10]
 	pkttype, seq, pktlen, src, Ndest, Rdest, dest1, dest2, dest3 = struct.unpack('BBHBBBBB', header)
+	data = pkt[10:].decode("utf-8")
+	contents = [pkttype, seq, pktlen, src, Ndest, Rdest, dest1, dest2, dest3, data]
+	return contents
 
 
 def read_pkt(pkt):
@@ -86,6 +91,7 @@ def read_pkt(pkt):
 	Inputs:
 		pkt:
 	Output:
+		contents
 	"""
 
 	if pkt[0] == 4: 
