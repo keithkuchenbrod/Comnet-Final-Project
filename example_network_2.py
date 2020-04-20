4eded6212b8f4413f4b17ad6b49d37fdd22e9c49
None <None>
2020-04-20 15:18:29 -0700

    Updated code on line 60

--- example_network_2.py
+++ example_network_2.py
@@ -11,6 +11,7 @@ from cleanup import cleanup
 from bootstrap import bootstrap
 import logging
 from utility import generate_routing_table
+import os
 
 def example_network_2():
     #Create new mininet
@@ -31,16 +32,20 @@ def example_network_2():
 
     #Establishing links
     info("Creating links\n")
-    net.addLink( s, r1 )
-    net.addLink( r1, r2 )
-    net.addLink( r1, r4 )
-    net.addLink( r2, r3 )
-    net.addLink( r3, d1 )
-    net.addLink( r4, r3 )
-    net.addLink( r4, r5 )
-    net.addLink( r5, r6 )
-    net.addLink( r6, d2 )
-    net.addLink( r6, d3 )
+    net.addLink( s, r1, intfName2='r1-eth0' )
+    net.addLink( r1, r2, intfName1='r1-eth1', intfName2='r2-eth0' )
+    net.addLink( r1, r4, intfName1='r1-eth2' )
+    net.addLink( r2, r3, intfName1='r2-eth1', intfName2='r3-eth0' )
+    net.addLink( r3, d1, intfName1='r3-eth1', intfName2='d1-eth0' )
+    net.addLink( r4, r3, intfName1='r4-eth1', intfName2='r3-eth2' )
+    net.addLink( r4, r5, intfName1='r4-eth2', intfName2='r5-eth0' )
+    net.addLink( r5, r6, intfName1='r5-eth1', intfName2='r6-eth0' )
+    net.addLink( r6, d2, intfName1='r6-eth1' )
+    net.addLink( r6, d3, intfName1='r6-eth2' )
+
+    #Setting interface ip addresses since params1 or params2 just will not work
+    #router1 = net.get( 'r1' )
+    #router1.setIP(
 
     #Set up forwarding on routers
     r1.cmd( 'sysctl net.ipv4.ip_forward=1' )
@@ -54,10 +59,14 @@ def example_network_2():
     routers = [r1, r2, r3, r4, r5, r6]
     hosts = [s, d1, d2, d3]
 
+    #Save ifconfig file
+    with open('ifconfig.txt','w') as fp:
+        fp.write(r1.cmd('ifconfig'))
+
     #Boostrap, starts scripts on each node
     info('Booting router scripts and generating routing tables')
     for router in routers:
-        generate_routing_table(router.cmd('route -n').split(), str(router))
+        generate_routing_table(str(router), net.links, 'ifconfig.txt')
     	router.cmd( 'python router.py {}'.format(str(router)) ) 
 
     info('Booting Host Scripts')
@@ -71,6 +80,7 @@ def example_network_2():
     CLI( net )
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='debug_logs/network_debug.log', level=logging.INFO)
     example_network_2()
     cleanup()
 
