18f71b5f647d975127aa1b8f7a30a6d77c27a1d9
Abhinandan Vellanki <35554825+AbhinandanVellanki@users.noreply.github.com>
2020-04-21 13:15:10 -0400

    updated router.py
    added code to enable forwarding, termination and started handling packets. Added code to send ack packet

--- router.py
+++ router.py
@@ -3,6 +3,11 @@ import logging
 from sys import argv
 import os
 import json
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
 
 class Router:
 
@@ -11,6 +16,20 @@ class Router:
 		self.id = argv[1]
 		#self.ip = argv[2] Do we need this??
 		self.routing_table = None
+        #To enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+        self.cmd( 'sysctl net.ipv4.icmp_echo_ignore_broadcasts=0' )
+        self.cmd( 'sysctl net.ipv4.conf.r0-eth1.force_igmp_version=2' )
+        self.cmd( 'sysctl net.ipv4.conf.r0-eth2.force_igmp_version=2' )
+        self.cmd( 'sysctl net.ipv4.conf.r0-eth3.force_igmp_version=2' )
+        self.cmd( '/opt/smcroute/sbin/smcrouted -l debug -I smcroute-r0' )
+        self.cmd( 'sleep 1')
+        self.cmd( '/opt/smcroute/sbin/smcroutectl -I smcroute-r0 '
+                  'add r0-eth1 239.0.0.1 r0-eth2 r0-eth3' )
+        self.cmd( '/opt/smcroute/sbin/smcroutectl -I smcroute-r0 '
+                  'add r0-eth2 239.0.0.2 r0-eth1 r0-eth3' )
+        self.cmd( '/opt/smcroute/sbin/smcroutectl -I smcroute-r0 '
+                  'add r0-eth3 239.0.0.3 r0-eth1 r0-eth2' )
 
 	def get_router_information(self):
 		"""Logs routing information in debug.log file"""
@@ -25,11 +44,34 @@ class Router:
 				with open('routing_tables/{}'.format(file), 'r') as fp:
 					self.routing_table = json.load(fp)
 
+    def terminate(self):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        self.cmd( '/opt/smcroute/sbin/smcroutectl -I smcroute-r0 kill' )
+
 	def bootstrap(self, network):
-		raise NotImplmentedError
+		raise NotImplementedError
+
+    def createACKpkt(src, dest): #can be removed once packets.py is imported
+	    seq = random.randint(0,254)
+	    return struct.pack('BBBB', 4, seq, src, dest)
+
+    def run(self):
+        sock=socket(AF_INET,SOCK_DGRAM)
+        sock.bind((self.ip,self.id))
+        while True:
+            packet,addr=sock.recvfrom(self.buffer_size)
+            header=read_header(packet)
+            if header(0) == 'Hello':#ignore Hello Packets
+                continue
+            elif header(0) == 'LSU':
+                if header(2)==self.id: #drop packet if sent from same router
+                    continue
+            sock.send(self.createACKpkt(self.id, addr))#send ack packet
+                    
 
 
-if __name__ == '__main__':
-	router = Router()
-	router.load_routing_table()
-	router.get_router_information()
+    if __name__=='__main__':
+        router=Router()
+        router.load_routing_table()
+        router.get_router_information
+        router.run()
